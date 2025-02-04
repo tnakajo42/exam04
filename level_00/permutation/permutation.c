b93c8c3cb027828fabde0feee517d6be88247343
Taka Nakajo <121729759+tnakajo42@users.noreply.github.com>
2025-02-04 12:28:28 +0100

    Update ft_alphabetical on permutation.c
    Update ft_alphabetical on permutation.c

--- level_00/permutation/permutation.c
+++ level_00/permutation/permutation.c
@@ -1,111 +1,70 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-// Function to swap two characters
-void swap(char *x, char *y) {
-    char temp = *x;
-    *x = *y;
-    *y = temp;
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-// Function to reverse a part of the string
-void reverse(char *str, int start, int end) {
-    while (start < end) {
-        swap(&str[start], &str[end]);
-        start++;
-        end--;
-    }
+void	ft_swap(char *x, char *y)
+{
+	char	temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
 }
 
-// Function to find the next lexicographical permutation
-int next_permutation(char *str, int len) {
-    int i = len - 2;
+void	ft_alphabetical(char *str, int len, int start)
+{
+	int	i;
+	int	j;
 
-    // Step 1: Find the largest index `i` such that str[i] < str[i+1]
-    while (i >= 0 && str[i] >= str[i + 1]) {
-        i--;
-    }
-
-    // If no such index exists, the permutation is the last one (sorted in descending order)
-    if (i < 0) {
-        return 0; // No more permutations
-    }
-
-    // Step 2: Find the largest index `j` such that str[i] < str[j]
-    int j = len - 1;
-    while (str[j] <= str[i]) {
-        j--;
-    }
-
-    // Step 3: Swap the values of str[i] and str[j]
-    swap(&str[i], &str[j]);
-
-    // Step 4: Reverse the sequence from str[i+1] to the end of the string
-    reverse(str, i + 1, len - 1);
-
-    return 1;
+	i = start;
+	while (i < len - 1)
+	{
+		j = start;
+		while (j < len - 1)
+		{
+			if (str[j] > str[j + 1])
+				ft_swap(&str[j], &str[j + 1]);
+			j++;
+		}
+		i++;
+	}
 }
 
-// Comparison function for qsort to compare individual characters
-int char_compare(const void *a, const void *b) {
-    return (*(char *)a - *(char *)b);
+void	ft_permute(char *str, int start, int end)
+{
+	int	i;
+
+	i = start;
+	if (str && start == end)
+		puts(str);
+	else
+	{
+		while (i <= end)
+		{
+			ft_alphabetical(str, end + 1, start);
+			ft_swap(&str[start], &str[i]);
+			ft_permute(str, start + 1, end);
+			ft_swap(&str[start], &str[i]);
+			i++;
+		}
+	}
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <string>\n", argv[0]);
-        return 1;
-    }
+int	main(int ac, char **av)
+{
+	int	i;
 
-    int len = strlen(argv[1]);
-
-    // Sort the input string first using correct character comparison
-    qsort(argv[1], len, sizeof(char), char_compare);
-
-    // Print the first (sorted) permutation
-    printf("%s\n", argv[1]);
-
-    // Generate and print all subsequent permutations in lexicographical order
-    while (next_permutation(argv[1], len)) {
-        printf("%s\n", argv[1]);
-    }
-
-    return 0;
+	if (ac != 2)
+		return (1);
+	i = ft_strlen(av[1]);
+	ft_permute(av[1], 0, i - 1);
+	return (0);
 }
-
-/*
-#include <stdio.h>
-#include <string.h>
-
-void swap(char *x, char *y) {
-    char temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-void permute(char *str, int start, int end) {
-    if (start == end) {
-        printf("%s\n", str);
-    } else {
-        for (int i = start; i <= end; i++) {
-            swap((str + start), (str + i));
-            permute(str, start + 1, end);
-            swap((str + start), (str + i)); // backtrack
-        }
-    }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <string>\n", argv[0]);
-        return 1;
-    }
-
-    int len = strlen(argv[1]);
-    permute(argv[1], 0, len - 1);
-
-    return 0;
-}
-*/
