0374895428f8354c0882923a0477f3bed78e40ce
Takayuki Nakajo <tnakajo@c4b12c1.42berlin.de>
2025-02-15 17:45:35 +0100

    make argo shorter and clear

--- level_02/argo/argo.c
+++ level_02/argo/argo.c
@@ -2,32 +2,32 @@
 #include <unistd.h> // for write()
 
 int peek(FILE *stream) {
-    int c = getc(stream);
+	int c = getc(stream);
 	ungetc(c, stream);
-    return c;
+	return c;
 }
 
 void unexpected(FILE *stream) {
-    if (peek(stream) != EOF)
-        printf("Unexpected token '%c'\n", peek(stream));
-    else
+	if (peek(stream) != EOF)
+		printf("Unexpected token '%c'\n", peek(stream));
+	else
 		write(1, "Unexpected end of input\n", 24); // CHANGED because fflush(stdout) is not allowed
 }
 
 int accept(FILE *stream, char c) {
-    if (peek(stream) == c) {
-        (void) getc(stream);
-        return 1;
-    }
-    return 0;
+	if (peek(stream) == c) {
+		(void) getc(stream);
+		return 1;
+	}
+	return 0;
 }
 
 int expect(FILE *stream, char c) {
-    if (accept(stream, c)) {
-        return 1;
-    }
-    unexpected(stream);
-    return 0;
+	if (accept(stream, c)) {
+		return 1;
+	}
+	unexpected(stream);
+	return 0;
 }
 
 //add your code from here below
@@ -93,13 +93,13 @@ int parse_string(char **dst, FILE *stream)
 int parse_map(json *dst, FILE *stream)
 {
 	dst->map.size = 0;
-    dst->map.data = NULL;
+	dst->map.data = NULL;
 	char *key;
-	
+
 	// Ensure opening brace is consumed
 	if (!expect(stream, '{'))
 		return -1;
-    while (peek(stream) != '}')
+	while (peek(stream) != '}')
 	{
 		if (dst->map.size > 0 && !expect(stream, ','))
 			return (-1);
@@ -113,30 +113,30 @@ int parse_map(json *dst, FILE *stream)
 			free(key);
 			return (-1);
 		}
-        // Parse value
-        json value;
-        if (argo(&value, stream) == -1)
+		// Parse value
+		json value;
+		if (argo(&value, stream) == -1)
 		{
-            free(key);
-            return (-1);
-        }
-        // Add key-value pair to map
-        dst->map.data = realloc(dst->map.data, sizeof(pair) * (dst->map.size + 1));
-        if (!dst->map.data)
+			free(key);
+			return (-1);
+		}
+		// Add key-value pair to map
+		dst->map.data = realloc(dst->map.data, sizeof(pair) * (dst->map.size + 1));
+		if (!dst->map.data)
 		{
-            free(key);
-            free_json(value);
+			free(key);
+			free_json(value);
 			free(dst->map.data);
 			return (-1);
 		}
 		dst->map.data[dst->map.size].key = key;
 		dst->map.data[dst->map.size].value = value;
 		dst->map.size++;
-    }
-    // Consume the closing brace
-    if (!expect(stream, '}'))
-        return (-1);
-    return (1);
+	}
+	// Consume the closing brace
+	if (!expect(stream, '}'))
+		return (-1);
+	return (1);
 }
 
 int argo(json* dst, FILE* stream)
